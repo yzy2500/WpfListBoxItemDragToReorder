b6f54ed6c72781646deb246b3eed6c23f18b5d95
yzy <yzy@DESKTOP-2JHJVMO>
2023-04-19 09:33:40 +0800

    添加项目文件。

---
+++ WpfListBoxItemDragToReorder/ListBoxItemDragBehavior.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfListBoxItemDragToReorder
+{
+    public class ListBoxItemDragBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.AssociatedObject.AddHandler(UIElement.MouseLeftButtonDownEvent, new MouseButtonEventHandler(OnMouseLeftButtonDown), handledEventsToo: false);
+        }
+
+        protected override void OnDetaching()
+        {
+            base.AssociatedObject.RemoveHandler(UIElement.MouseLeftButtonDownEvent, new MouseButtonEventHandler(OnMouseLeftButtonDown));
+        }
+
+        private async void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is not FrameworkElement f) return;
+            Panel? panel = null;
+            ListBox? listBox = null;
+            DependencyObject? obj = f;
+            while (obj is FrameworkElement)
+            {
+                obj = VisualTreeHelper.GetParent(obj);
+                if (obj is ListBox box) listBox = box;
+                else if (panel == null) 
+                {
+                    if (obj is Panel p) panel = p;
+                    else if (obj is FrameworkElement t) f = t;
+                }
+            }
+            if (panel == null || listBox == null || listBox.ItemsSource is not IList list || !list.Contains(f.DataContext)) return;
+
+            var old = f.RenderTransform;
+            var z = Panel.GetZIndex(panel);
+            try
+            {
+                Panel.SetZIndex(f, 999);
+                MatrixTransform matrix = new MatrixTransform(old.Value);
+                TranslateTransform translate = new TranslateTransform();
+                TransformGroup group = new TransformGroup();
+                group.Children.Add(matrix);
+                group.Children.Add(translate);
+                f.RenderTransform = group;
+
+                var start = Mouse.GetPosition(panel);
+                var end = start;
+                while (Mouse.LeftButton == MouseButtonState.Pressed)
+                {
+                    await Task.Delay(20);
+                    end = Mouse.GetPosition(panel);
+                    translate.Y = end.Y - start.Y;
+                }
+
+                var data = f.DataContext;
+                var index = list.IndexOf(data);
+                var h = f.ActualHeight;
+                var order = (int)(((index + 0.5) * h + translate.Y) / h + 0.5);
+                if (index < order) order--;
+                list.Remove(data);
+                list.Insert(order, data);
+            }
+            finally
+            {
+                Panel.SetZIndex(f, z);
+                f.RenderTransform = old;
+            }
+        }
+    }
+}
---
+++ WpfListBoxItemDragToReorder/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfListBoxItemDragToReorder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        ObservableCollection<int> list = new ObservableCollection<int>();
+        private void ListBox_Initialized(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 100; i++) list.Add(i);
+            if (sender is ListBox box) box.ItemsSource = list;
+        }
+    }
+}
